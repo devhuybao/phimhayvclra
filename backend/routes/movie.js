916110f57c23c1124ae938ca173c670b2ff12c7a
devhuybao <bhuy8305@gmail.com>
2026-02-13 00:04:12 +0700

    Update movie.js

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -1,1 +1,21 @@
+const router = require("express").Router()
+const Movie = require("../models/Movie")
+
+router.post("/add", async (req, res) => {
+  const movie = new Movie(req.body)
+  await movie.save()
+  res.json("movie added")
+})
+
+router.get("/all", async (req, res) => {
+  const movies = await Movie.find()
+  res.json(movies)
+})
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id)
+  res.json(movie)
+})
+
+module.exports = router
 
