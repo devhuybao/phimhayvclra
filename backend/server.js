94b704c40e42ed022ad056c047bda256a1adf8bb
devhuybao <bhuy8305@gmail.com>
2026-02-12 23:56:12 +0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/motchill")
+
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/movie", require("./routes/movie"))
+
+app.listen(5000, () =>
+  console.log("Server running http://localhost:5000")
+)
