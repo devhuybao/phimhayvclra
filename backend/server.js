25be60f6682c270aa7a24d2133c673c0307afe4e
devhuybao <bhuy8305@gmail.com>
2026-02-13 00:24:01 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/moviestream")
+mongoose.connect("mongodb+srv://admin:<db_password>@cluster0.iizzvqy.mongodb.net/?appName=Cluster0")
 
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/movie", require("./routes/movie"))
