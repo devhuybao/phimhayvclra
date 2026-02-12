c5af0f23402c2cffe8f79276071004d5e55c063e
devhuybao <bhuy8305@gmail.com>
2026-02-13 00:02:24 +0700

    Update Movie.js

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose")
 
+module.exports = mongoose.model("Movie", {
+  title: String,
+  description: String,
+  videoUrl: String,
+  thumbnail: String,
+  createdAt: { type: Date, default: Date.now }
+})
