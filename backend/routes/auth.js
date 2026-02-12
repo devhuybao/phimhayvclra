87ddc671d6fe8276109a293d27f735b1e6e201c4
devhuybao <bhuy8305@gmail.com>
2026-02-13 00:02:49 +0700

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,42 @@
+const router = require("express").Router()
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
+const SECRET = "secret_key"
+
+router.post("/register", async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, 10)
+
+  const user = new User({
+    username: req.body.username,
+    password: hash
+  })
+
+  await user.save()
+  res.json("registered")
+})
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({
+    username: req.body.username
+  })
+
+  if (!user) return res.json("no user")
+
+  const valid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  )
+
+  if (!valid) return res.json("wrong pass")
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    SECRET
+  )
+
+  res.json({ token, role: user.role })
+})
+
+module.exports = router
